0cdd82233e54ef5dc53d3eb1a554a67aedd7774e
Jose R Macedo Junior <junior76macedo4@gmail.com>
2022-08-03 18:36:39 -0300

    first commit

---
+++ ex5-8.py
@@ -0,0 +1,37 @@
+'''
+Antecessor e sucessor
+
+a =int(input('Escolha um número: '))
+su =a+1
+an =a-1
+print('O seu número é {}, seu sucessor é {} e seu antecessor é {}'.format(a,su, an))
+
+'''
+
+'''
+Dobro, triplo e raíz quadrada
+
+n1 =int(input('Escolha um número: '))
+dobro =n1*2
+triplo =n1*3
+rq =n1**2
+print('O seu número é {}, o dobro dele é {}, seu triplo é {}, e a sua raíz quadrada é {}'.format(n1,dobro,triplo,rq))
+'''
+
+'''
+Média aritmética
+
+nota1 =int(input('Primeira nota do aluno: '))
+nota2 =int(input('Segunda nota do aluno: '))
+media =(nota1+nota2)/2
+print('A média do aluno é: ',media)
+'''
+
+'''
+Conversor de medidas
+
+medida=float(input('Distancia em metros: '))
+cm = medida*100
+mm = medida*1000
+print('A medida corresponde a {:.0f} metros, {:.0f} cm e {:.0f} mm'.format(medida,cm,mm))
+'''
